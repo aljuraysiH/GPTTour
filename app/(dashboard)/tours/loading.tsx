378bb9f316fc96f274c139a1b0d75371e784c9c0
hamad <aljuraysih@gmail.com>
2025-04-29 12:23:36 +0300

    remove semicolon

--- app/(dashboard)/tours/loading.tsx
+++ app/(dashboard)/tours/loading.tsx
@@ -1,7 +1,7 @@
 const loading = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <span className="loading loading-lg"></span>;
+      <span className="loading loading-lg"></span>
     </div>
   );
 };
